9db93143114ee714c048dafde0eb616d1ba2b8bd
Harriet Lee <lharriet27@gmail.com>
2020-11-28 23:45:03 +0900

    -Baekjoon

---
+++ codingTest/codingTest/TestFile.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//baekjoon 2557
+	//std::cout << "Hello World!";
+
+	//baekjoon 10718
+	//std::cout << "강한친구 대한육군" << '\n';
+	//std::cout << "강한친구 대한육군";
+
+	//baekjoon 10171
+	/*cout << "\\    /\\"<< '\n';
+	cout << " )  ( ')"<< '\n';
+	cout << "(  /  )"<< '\n';
+	cout << " \\(__)|";*/
+
+	//baekjoon 1000
+	int A, B;
+	cin >> A >> B;
+	//cout << A + B;
+
+	//baekjoon 1001
+	//cout << A - B;
+	
+	//baekjoon 1008
+	cout.precision(10);
+	cout << (double)A / B;
+
+	return 0;
+
+}
