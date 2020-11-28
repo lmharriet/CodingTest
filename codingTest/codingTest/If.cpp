8455b37407405ae4165374dc2f0f536a4549288b
Harriet Lee <lharriet27@gmail.com>
2020-11-29 01:58:09 +0900

    Baekjoon IF

---
+++ codingTest/codingTest/If.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//baekjoon 1330
+
+	//int A,B;
+	//cin >> A>> B;
+
+	//if (A > B) cout << ">";
+	//else if (A < B) cout << "<";
+	//else cout << "==";
+
+	//baekjoon 9498
+
+	//int score;
+	//cin >> score;
+	//if (score >= 90) cout << "A";
+	//else if (score >= 80) cout << "B";
+	//else if (score >= 70) cout << "C";
+	//else if (score >= 60) cout << "D";
+	//else cout << "F";
+
+
+	//baekjoon 2753
+
+	/*bool isLeapYear;
+	int year;
+	cin >> year;
+	if (year % 4 == 0 && year % 100 != 0 || year % 400==0)
+	{
+		isLeapYear = true;
+	}
+	else isLeapYear = false;
+
+	cout << isLeapYear;*/
+
+
+	//baekjoon 14681
+
+	//int axisX, axisY;
+	//cin >> axisX>> axisY;
+
+	//if (axisX > 0 && axisY > 0) cout << "1";
+	//else if (axisX < 0 && axisY > 0) cout << "2";
+	//else if (axisX < 0 && axisY < 0) cout << "3";
+	////else if (axisX > 0 && axisY < 0) cout << "4";
+	//else cout << "4";
+
+
+	//baekjoon 2884
+
+	int H, M;
+	cin >> H >> M;
+	
+	M -= 45;
+	if (H < 0) H += 24;
+	if (M<0)
+	{	
+		H--;
+		M += 60;
+	}
+	cout << H << " " << M;
+
+	return 0;
+}
--- codingTest/codingTest/TestFile.cpp
+++ codingTest/codingTest/TestFile.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 using namespace std;
-int main()
+int main1()
 {
+
 	//baekjoon 2557
 	//std::cout << "Hello World!";
 
@@ -18,15 +19,23 @@ int main()
 
 	//baekjoon 1000
 	int A, B;
-	cin >> A >> B;
+	cin >> A >> B; 
 	//cout << A + B;
 
 	//baekjoon 1001
 	//cout << A - B;
 	
 	//baekjoon 1008
-	cout.precision(10);
-	cout << (double)A / B;
+	//cout.precision(10); //유효숫자 설정
+	//cout << (double)A / B;
+
+	//baekjoon 10869
+	
+	cout << A + B << '\n';
+	cout << A - B << '\n';
+	cout << A * B << '\n';
+	cout << A / B << '\n';
+	cout << A % B;
 
 	return 0;
 
