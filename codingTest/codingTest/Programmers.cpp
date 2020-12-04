16a15ccc2e0c8ac186771eb18fe61d0b719119c5
Harriet Lee <lharriet27@gmail.com>
2020-12-04 20:52:02 +0900

    programmers level 1

--- codingTest/codingTest/Programmers.cpp
+++ codingTest/codingTest/Programmers.cpp
@@ -5,40 +5,42 @@
 //가운데 글자 가져오기
 using namespace std;
 
-string solution(string s) {
-	string answer = "";
+#include <string>
+#include <vector>
 
-	int len = s.length();
+using namespace std;
 
-	if (len % 2 == 0)
+//문자열을 정수로 바꾸기
+
+int solution(string s)
+{
+
+	int answer = 0;
+
+	if (s[0] == '-')
 	{
-		answer += s[len / 2 - 1];
-		// '' + 'c' = 'c'
-		answer += s[len / 2];
-		//answer = 'c' + 'd'
-		// 'c' + 'd' = "cd"
+		if (s[1] == '0') s.erase(0, 2);
+		else s.erase(0, 1);
+		answer = stoi(s, nullptr, 10);
+		answer *= -1;
 	}
-	// 홀수
 	else
 	{
-		// 0 1 2 3 4
-		// a b c d e
+		if (s[0] == '0') s.erase(0, 1);
+		answer = stoi(s, nullptr, 10);
 
-		// 5 / 2 = 2 (2번 인덱스가 중앙)
-
-		// 0 1 2
-		// a b c
-
-		// 3 / 2 = 1
-
-		answer = s[len / 2];
 	}
 
-	return answer;
+		return answer;
 }
+
 int main()
 {
-	cout << solution("qwert");
-
+	cout << solution("034");
+	//string a = "1234";
+	//
+	//int num =stoi(a, nullptr, 10);
+	//cout << num;
+	//
 	return 0;
 }
