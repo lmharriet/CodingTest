7fa8ffec6b4d460568dc61a58af2d257fc44ca81
Harriet Lee <lharriet27@gmail.com>
2020-12-04 20:52:50 +0900

    programmers level1 stoi()

--- codingTest/codingTest/Programmers.cpp
+++ codingTest/codingTest/Programmers.cpp
@@ -14,23 +14,7 @@ using namespace std;
 
 int solution(string s)
 {
-
-	int answer = 0;
-
-	if (s[0] == '-')
-	{
-		if (s[1] == '0') s.erase(0, 2);
-		else s.erase(0, 1);
-		answer = stoi(s, nullptr, 10);
-		answer *= -1;
-	}
-	else
-	{
-		if (s[0] == '0') s.erase(0, 1);
-		answer = stoi(s, nullptr, 10);
-
-	}
-
+	int answer = stoi(s);
 		return answer;
 }
 
