0956e336cfca69776872d093986639800028d276
Harriet Lee <lharriet27@gmail.com>
2020-12-08 18:26:08 +0900

    programmers level1 descendingOrdering

--- codingTest/codingTest/Programmers.cpp
+++ codingTest/codingTest/Programmers.cpp
@@ -3,34 +3,29 @@
 #include <vector>
 
 using namespace std;
-
+//
 vector<int> solution(long long n) {
     vector<int> answer;
 
-    string str = to_string(n);
-
-    //for(int i = 0; i < max; i++)
-
-    //"12345" length = 5
-
-
-    for (int i = str.length()-1; i >= 0; i--)
+    while (n > 0)
     {
-        int result = str[i] - '0';
+        int fnum = n % 10;
+        n /= 10;
 
-        answer.push_back(result);
+        answer.push_back(fnum);
     }
+
     return answer;
 }
 
-int main()
-{
-    vector<int> v1 = solution(12345);
-
-    for (int i = 0; i < v1.size(); i++)
-    {
-        cout << v1[i];
-    }
-
-    return 0;
-}
+//int main()
+//{
+//    vector<int> v1 = solution(12345);
+//
+//    for (int i = 0; i < v1.size(); i++)
+//    {
+//        cout << v1[i];
+//    }
+//
+//    return 0;
+//}
