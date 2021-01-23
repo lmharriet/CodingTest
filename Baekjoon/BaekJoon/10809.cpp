54ea0b038f32cd1afa81314689f9a12937eef4c4
Harriet Lee <lharriet27@gmail.com>
2021-01-23 23:17:32 +0900

    baekjoon 10809

---
+++ Baekjoon/BaekJoon/10809.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+/*
+알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 
+단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 
+포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+
+
+a b c d e f g h i j k l m n o p q r s t u v w x y z
+ 
+ baekjoon
+ 
+ 012345 7 ( o가 두 번이고 처음 등장하는 위치는 5번 이므로 5를 사용)
+*/
+int FindAlphabet()
+
+{
+	return 0;
+}
