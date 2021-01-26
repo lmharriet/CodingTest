43e214f705669060d240dccfc912db283edde369
Harriet Lee <lharriet27@gmail.com>
2021-01-26 22:53:33 +0900

    baekjoon 5622 dialNumber

---
+++ Baekjoon/BaekJoon/5622.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int dialNumber()
+{
+	string input;
+	string dialStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	//A B C D E F G H I J K L M N O P Q R S T U V  W  X  Y  Z
+	//3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 8 9 9 9 10 10 10 10
+
+	int dial[26] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
+	int sum=0;
+
+	cin >> input;
+
+	for (int i = 0; i < input.length(); i++)
+	{
+		for (int j =0;j<dialStr.length();j++)
+		{
+			if (input[i] == dialStr[j])
+			{
+				cout << dial[j] << '\n';
+				sum += dial[j];
+				break;
+			}
+
+		}
+	}
+
+	cout << sum;
+
+	return 0;
+}
