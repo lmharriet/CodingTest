740342ca9aea927383a36abfc06d0704e08ac08c
Harriet Lee <lharriet27@gmail.com>
2021-01-26 23:07:52 +0900

    modified 5622

--- Baekjoon/BaekJoon/5622.cpp
+++ Baekjoon/BaekJoon/5622.cpp
@@ -10,18 +10,17 @@ int dialNumber()
 	//A B C D E F G H I J K L M N O P Q R S T U V  W  X  Y  Z
 	//3 3 3 4 4 4 5 5 5 6 6 6 7 7 7 8 8 8 8 9 9 9 10 10 10 10
 
-	int dial[26] = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
-	int sum=0;
+	int dial[26] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
+	int sum = 0;
 
 	cin >> input;
-
-	for (int i = 0; i < input.length(); i++)
+	int inputSize = input.length();
+	for (int i = 0; i < inputSize; i++)
 	{
-		for (int j =0;j<dialStr.length();j++)
+		for (int j = 0; j < 26; j++)
 		{
-			if (input[i] == dialStr[j])
+			if (input[i] == dialStr[j]) 
 			{
-				cout << dial[j] << '\n';
 				sum += dial[j];
 				break;
 			}
