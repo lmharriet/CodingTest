715db620fc7089f1b83a38b3dbc93aea61350cd1
Harriet Lee <lharriet27@gmail.com>
2021-01-27 23:18:50 +0900

    baekjoon 11720 string

---
+++ Baekjoon/BaekJoon/11720.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+
+/*
+N개의 숫자가 공백 없이 쓰여있다. 이 숫자를 모두 합해서 출력하는 프로그램을 작성하시오.
+
+ex)
+입력
+5
+54321
+
+출력
+15
+*/
+using namespace std;
+int main()
+{
+	int N;
+	cin >> N;
+	char* number = new char[N];
+
+	int total = 0;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> number[i];
+		total += (number[i] - 48);
+	}
+
+	cout << total;
+	return 0;
+}
