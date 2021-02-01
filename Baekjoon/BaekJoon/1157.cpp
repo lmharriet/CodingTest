f3d3f44fc459429e1b8a63a95a7f5eca24976e21
Harriet Lee <lharriet27@gmail.com>
2021-02-01 17:23:03 +0900

    changed function name

--- Baekjoon/BaekJoon/1157.cpp
+++ Baekjoon/BaekJoon/1157.cpp
@@ -12,7 +12,7 @@ using namespace std;
 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 
 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
 */
-int main()
+int StudyVoca()
 {
 	string word;
 	int alphabet[26] = { 0, };
