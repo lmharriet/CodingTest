0040f8a868cf0cd4a9997e77754345039c198259
Harriet Lee <lharriet27@gmail.com>
2021-02-15 00:14:47 +0900

    marathon

---
+++ ProgrammersLevel1/ProgrammersLevel1/marathon.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+	string answer = "";
+
+	int size = participant.size();
+	//   key  value
+	map<string,int> m;
+	for (int i = 0; i < size; i++)
+	{
+		string name = participant[i];
+		m[name]++;
+	}
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		string name = completion[i];
+		m[name]--;
+	}
+
+	map<string, int>::iterator iter;
+	for (iter = m.begin(); iter != m.end(); ++iter)
+	{
+		if (iter->second != 0)
+		{
+			answer = iter->first;//+=,=
+			break;
+		}
+	}
+	
+	return answer;
+}
+
+int main()
+{
+	cout << solution({ "leo", "kiki", "eden" }, { "eden", "kiki" }) << '\n';
+	cout << solution({ "marina", "josipa", "nikola","vinko","filipa" }, { "josipa", "filipa", "marina", "nikola" }) << '\n';
+	cout << solution({ "mislav", "stanko", "mislav","ana" }, { "stanko", "ana","mislav" }) << '\n';
+
+	return 0;
+}
