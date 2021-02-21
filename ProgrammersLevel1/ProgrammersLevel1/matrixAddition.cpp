caf5dc48ad9c141961553413e45e0886756deec4
Harriet Lee <lharriet27@gmail.com>
2021-02-21 19:57:21 +0900

    [matrixAdd] other method to solve

--- ProgrammersLevel1/ProgrammersLevel1/matrixAddition.cpp
+++ ProgrammersLevel1/ProgrammersLevel1/matrixAddition.cpp
@@ -3,36 +3,51 @@
 
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+vector<vector<int>> matrixAdd(vector<vector<int>> arr1, vector<vector<int>> arr2) {
 	vector<vector<int>> answer;
 
-	int size = arr1.size();
+	//int size = arr1.size();
 
-	for (int i = 0; i < size; i++)
+	//for (int i = 0; i < size; i++)
+	//{
+	//	answer.emplace_back(arr1[i]);
+	//	for (int j = 0; j < arr1[i].size(); j++)
+	//	{
+	//		answer[i][j] = arr1[i][j] + arr2[i][j];
+	//	}
+
+	//}
+
+	vector<int> temp;
+
+	for (int i = 0; i < arr1.size(); i++)
 	{
-		answer.emplace_back(arr1[i]);
 		for (int j = 0; j < arr1[i].size(); j++)
 		{
-			answer[i][j] = arr1[i][j] + arr2[i][j];
-		}
+			temp.emplace_back(arr1[i][j] + arr2[i][j]);
 
+		}
+		answer.emplace_back(temp);
+		temp.clear();
 	}
+
+
 
 	return answer;
 }
-
-int main()
-{
-	vector<vector<int>> v= solution({ {1}, {2} }, { {3},{4} });
-
-	for (int i = 0; i < v.size(); i++)
-	{
-		for (int j = 0; j < v[i].size(); j++)
-		{
-			cout << v[i][j]<<", ";
-		}
-		cout << '\n';
-	}
-	//solution({ {1}, {2} }, { {3},{4} });
-	//solution({ {1,2}, {2,3} }, { {3,4},{5,6} });
-}
+//
+//int main()
+//{
+//	vector<vector<int>> v = matrixAdd({ {1}, {2} }, { {3},{4} });
+//
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		for (int j = 0; j < v[i].size(); j++)
+//		{
+//			cout << v[i][j] << ", ";
+//		}
+//		cout << '\n';
+//	}
+//	//solution({ {1}, {2} }, { {3},{4} });
+//	//solution({ {1,2}, {2,3} }, { {3,4},{5,6} });
+//}
