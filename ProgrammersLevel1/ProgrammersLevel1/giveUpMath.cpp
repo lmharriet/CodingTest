1ecc2488e1abf2e50351fa6988e03fbc1fa6c224
Harriet Lee <lharriet27@gmail.com>
2021-02-23 14:08:12 +0900

    programmers lv1 MathTest

---
+++ ProgrammersLevel1/ProgrammersLevel1/giveUpMath.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+vector<int> MathTest(vector<int> answers)
+{
+	vector<int> answer;
+	vector<vector<int>> person = { {1,2,3,4,5} ,{2,1,2,3,2,4,2,5 } ,{3,3,1,1,2,2,4,4,5,5} };
+	int counts[3] = { 0,0,0 };
+	int count = 0;
+	for (int i = 0; i < person.size(); i++)
+	{
+		for (int j = 0; j < answers.size(); j++)
+		{
+			int modular = j % (person[i].size());
+			if (person[i][modular] == answers[j])
+			{
+				counts[i]++;
+			}
+		}
+		count = 0;
+	}
+
+
+	int max = counts[0];
+
+	for (int i = 1; i < 3; i++)
+	{
+		if (counts[i] > max) max = counts[i];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		if (counts[i] == max)
+		{
+			answer.push_back(i + 1);
+		}
+	}
+
+	return answer;
+}
+
+//int main()
+//{
+//	vector<int> v = MathTest({1,2,3,4,5,1,2,3,4,5,1,2,3,4,5 });
+//
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		cout << v[i];
+//	}
+//	return 0;
+//}
