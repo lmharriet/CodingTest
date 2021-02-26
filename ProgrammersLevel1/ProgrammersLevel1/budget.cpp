bb75d02a6a1ed231619b96135af6520add632f0e
Harriet Lee <lharriet27@gmail.com>
2021-02-26 17:35:47 +0900

    change function name

--- ProgrammersLevel1/ProgrammersLevel1/budget.cpp
+++ ProgrammersLevel1/ProgrammersLevel1/budget.cpp
@@ -5,29 +5,28 @@
 #include <algorithm>
 using namespace std;
 
-int solution(vector<int> d, int budget) {
+int Budget(vector<int> d, int budget) {
 	int answer = 0;
 	int size = d.size();
 	sort(d.begin(), d.end());
-	
+
 	for (int i = 0; i < size; i++)
 	{
-		if (budget -d[i]>= 0)
+		if (budget - d[i] >= 0)
 		{
 			budget -= d[i];
 			answer++;
 		}
 	}
+
 	return answer;
 }
-
-int main()
-{
-
-	
-	//solution({ 11,3,2,5,6,1 }, 9);
-	cout << solution({ 11,3,2,5,6,1 }, 9) << '\n';
-	cout << solution({ 1,3,2,5,4 }, 9) << '\n';
-	cout << solution({ 2,2,3,3 }, 10);
-	return 0;
-}
+//
+//int main()
+//{
+//	//Budget({ 11,3,2,5,6,1 }, 9);
+//	cout << Budget({ 11,3,2,5,6,1 }, 9) << '\n';
+//	cout << Budget({ 1,3,2,5,4 }, 9) << '\n';
+//	cout << Budget({ 2,2,3,3 }, 10);
+//	return 0;
+//}
