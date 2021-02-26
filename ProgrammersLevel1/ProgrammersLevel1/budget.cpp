6394faac2fcaba7fe91a1917de6223b265b62400
Harriet Lee <lharriet27@gmail.com>
2021-02-26 16:16:33 +0900

    programmers budget cpp file

---
+++ ProgrammersLevel1/ProgrammersLevel1/budget.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    return answer;
+}
+
+int main()
+{
+    return 0;
+}
