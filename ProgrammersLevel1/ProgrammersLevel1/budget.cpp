898a8a600a7b0e0e5f324569d6425a307b4c068c
Harriet Lee <lharriet27@gmail.com>
2021-02-26 17:27:31 +0900

    programmers level1 budget

--- ProgrammersLevel1/ProgrammersLevel1/budget.cpp
+++ ProgrammersLevel1/ProgrammersLevel1/budget.cpp
@@ -2,15 +2,32 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<int> d, int budget) {
-    int answer = 0;
-    return answer;
+	int answer = 0;
+	int size = d.size();
+	sort(d.begin(), d.end());
+	
+	for (int i = 0; i < size; i++)
+	{
+		if (budget -d[i]>= 0)
+		{
+			budget -= d[i];
+			answer++;
+		}
+	}
+	return answer;
 }
 
 int main()
 {
-    return 0;
+
+	
+	//solution({ 11,3,2,5,6,1 }, 9);
+	cout << solution({ 11,3,2,5,6,1 }, 9) << '\n';
+	cout << solution({ 1,3,2,5,4 }, 9) << '\n';
+	cout << solution({ 2,2,3,3 }, 10);
+	return 0;
 }
