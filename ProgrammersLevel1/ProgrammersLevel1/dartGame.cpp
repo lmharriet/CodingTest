5e0b2df50f2b95468f3997b6fb48acdfb786a041
Harriet Lee <lharriet27@gmail.com>
2021-03-01 19:49:12 +0900

    programmers level 1 dartGame

--- ProgrammersLevel1/ProgrammersLevel1/dartGame.cpp
+++ ProgrammersLevel1/ProgrammersLevel1/dartGame.cpp
@@ -1,101 +1,83 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-int getPowNum(char sdt)
-{
-	return 0;
-}
-
 int solution(string dartResult) {
 	int answer = 0;
 	int length = dartResult.length();
-	vector<int> score;
-	int num[3] = { 0, };
-	// s ^1 d ^2 t ^3
-	//나 이전 2배<- 나 2배(처음에 나오면 처음것만 2배) 
-	//
+	int score[3] = { 0, };
+
+	int preScore = 0;
+	int chance = 0;
+
+	
+
 	for (int i = 0; i < length; i++)
 	{
-		if (dartResult[i] == 83) // S
+		if (dartResult[i] >= '0' && dartResult[i] <= '9')
 		{
-			if (dartResult[i - 1] == 48) // S인덱스 -1 이 0이면
+			if (i != 0)
+				chance++;
+
+			if (dartResult[i + 1] == '0')
 			{
-				if (dartResult[i - 2] == 49) // 10이면 S인덱스 -2 가 1
-				{
-					score.emplace_back(pow(10, 1));
-				}
-				else //그냥 점수가 0일 경우
-				{
-					score.emplace_back(0);
-				}
+				preScore = 10;
+				i++;
 			}
-			else if (dartResult[i - 1] >= 48 && dartResult[i - 1] <= 57)
+			else
 			{
-				score.emplace_back(pow(dartResult[i - 1] - 48, 1));
-
-			}
-
-		}
-		else if (dartResult[i] == 68) // D
-		{
-
-			if (dartResult[i - 1] == 48) // S인덱스 -1 이 0이면
-			{
-				if (dartResult[i - 2] == 49) // 10이면 S인덱스 -2 가 1
-				{
-					score.emplace_back(pow(10, 2));
-				}
-				else //그냥 점수가 0일 경우
-				{
-					score.emplace_back(0);
-				}
-			}
-			else if (dartResult[i - 1] >= 48 && dartResult[i - 1] <= 57)
-			{
-				score.emplace_back(pow(dartResult[i - 1] - 48, 2));
-
-			}
-
-		}
-		else if (dartResult[i] == 84) // T
-		{
-			if (dartResult[i - 1] == 48) // S인덱스 -1 이 0이면
-			{
-				if (dartResult[i - 2] == 49) // 10이면 S인덱스 -2 가 1
-				{
-					score.emplace_back(pow(10, 3));
-				}
-				else //그냥 점수가 0일 경우
-				{
-					score.emplace_back(0);
-				}
-			}
-			else if (dartResult[i - 1] >= 48 && dartResult[i - 1] <= 57)
-			{
-				score.emplace_back(pow(dartResult[i - 1] - 48, 3));
-
+				preScore = dartResult[i] - '0';
 			}
 		}
-
-
+		else if (dartResult[i] == 'S')
+		{
+			score[chance] = pow(preScore, 1);
+		}
+		else if (dartResult[i] == 'D')
+		{
+			score[chance] = pow(preScore, 2);
+		}
+		else if (dartResult[i] == 'T')
+		{
+			score[chance] = pow(preScore, 3);
+		}
+		else if (dartResult[i] == '*')
+		{
+			if (chance !=0)
+			{
+				score[chance] *= 2;
+				score[chance -1] *= 2;
+			}
+			else
+			{
+				score[chance] *= 2;
+			}
+		}
+		else if (dartResult[i] == '#')
+		{
+			score[chance] *= -1;
+		}
 	}
-
-
-	for (int i = 0; i < score.size(); i++)
+	for (int i = 0; i < 3; i++) 
 	{
-		cout << score[i] << '\n';
+		answer += score[i];
 	}
+
+	//cout << score[0] << " , "<<score[1] << " , " << score[2] << " :" << answer << '\n';
+
 	return answer;
 }
 
 int main()
 {
-	//점수 |보너스 |옵션
-	solution("1S2D*3T") << '\n';
-	solution("1D#0S*1T*");
+	cout << solution("1S2D*3T") << '\n';
+	cout << solution("1D2S#10S") << '\n';
+	cout << solution("1D2S0T") << '\n';
+	cout << solution("1S*2T*3S") << '\n';
+	cout << solution("1D#2S*3S") << '\n';
+	cout << solution("1T2D3D#") << '\n';
+	cout << solution("1D2S3T*") << '\n';
 	return 0;
 }
