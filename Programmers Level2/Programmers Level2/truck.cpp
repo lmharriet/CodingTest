74a500a2931d3e70bddc9478a9dfcd875dd5d087
Harriet Lee <lharriet27@gmail.com>
2021-04-03 00:18:24 +0900

    programmers lv2 Truck

--- Programmers Level2/Programmers Level2/truck.cpp
+++ Programmers Level2/Programmers Level2/truck.cpp
@@ -6,101 +6,48 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-
-	int seconds = 0;
+	int second = 0;
+	int truckIdx = 0;
 	int truckWeight = 0;
-	int count = truck_weights.size();
-	queue<int> truck_q;
-	queue<int> leftLength_q;
-
 	queue<int> q;
-
-	int i = 0;
-
-
 	while (true)
 	{
-		if (count == 0) break;
-		if (truck_q.empty())
+		second++;
+
+		if (q.size() == bridge_length)
 		{
-			//	지나는 트럭이 없음
-			// 트럭을 도로에 던지기
-			truck_q.push(truck_weights[i]);
-			leftLength_q.push(bridge_length);
-			truckWeight = truck_weights[i];
-			seconds++;
-			i++;
+			truckWeight -= q.front();
+			q.pop();
+			second++;
+		}
+
+		if (truckWeight + truck_weights[truckIdx] <= weight)
+		{
+			if (truckIdx == truck_weights.size() - 1)
+			{
+				second += bridge_length;
+				break;
+			}
+			q.push(truck_weights[truckIdx]);
+			truckWeight += truck_weights[truckIdx];
+			truckIdx++;
+		
 		}
 		else
 		{
-			if (i == truck_weights.size())
-			{
-				seconds += leftLength_q.front();
-				break;
-			}
-			// 지나는 트럭이 있음
-			if (leftLength_q.front() > 0)
-			{
-				leftLength_q.front()--;
-			}
-			else
-			{
-				truckWeight -= truck_q.front();
-				truck_q.pop();
-				leftLength_q.pop();
-				count--;
-			}
-			
-			if (truckWeight + truck_weights[i] <= weight)
-			{
-				truck_q.push(truck_weights[i]);
-				leftLength_q.push(bridge_length);
-				truckWeight += truck_weights[i];
-				i++;
-			}
-			else
-			{
-				seconds += leftLength_q.front();
-				truck_q.pop();
-				leftLength_q.pop();
-				count--;
-			}
+			q.push(0);
 		}
 
 	}
-
-
-	/*
-
-		q = truck_q;
-		cout << "현재 answer값 : " << answer << '\n';
-		while (!q.empty())
-		{
-			cout << "truck: " << q.front() << '\n';
-			q.pop();
-
-		}
-		q = leftLength_q;
-		while (!q.empty())
-		{
-			cout << "leftlength : " << q.front() << '\n';
-			q.pop();
-
-		}*/
-
-
-
-	cout << "answer: " << seconds;
-
-
-
-	return seconds;
+	return second;
 }
 
-int main()
-{
 
-	solution(2, 10, { 7,4,5,6 });
-	//solution(100, 100, { 10 });
-	return 0;
-}
+//
+//int main()
+//{
+//	cout<<solution(2, 10, { 7,4,5,6 }) << '\n';
+//	cout<<solution(100, 100, { 10,10,10,10,10,10,10,10,10,10 })<<'\n';
+//	cout<<solution(100, 100, { 10 });
+//	return 0;
+//}
