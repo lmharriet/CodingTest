216456e63c046c7b50abc7b55acd3d1d554d3153
Harriet Lee <lharriet27@gmail.com>
2021-04-03 00:17:38 +0900

    programmers lv2 phone_book (time over)

--- Programmers Level2/Programmers Level2/phoneBook.cpp
+++ Programmers Level2/Programmers Level2/phoneBook.cpp
@@ -3,40 +3,41 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 bool compare(string a, string b)
 {
 	return a.length() < b.length();
 }
 bool solution(vector<string> phone_book) {
-	//¹Ì¿Ï¼º
-	int size = phone_book.size();
+	//not finished yet (this was falied because of time over)
+	bool answer = true;
 
 	sort(phone_book.begin(), phone_book.end(), compare);
-	for (auto& it : phone_book)
-	{
-		cout << it << " , ";
-	}
 
-	cout << '\n';
-	for (int i = 0; i < size - 1; i++)
+	for (int i = 0; i < phone_book.size(); i++)
 	{
-		for (int j = i + 1; j < size; j++)
+		for (int j = 0; j < phone_book.size(); j++)
 		{
-			if (phone_book[j].find(phone_book[i]) == 0)
+			if (i == j) continue;
+			if (phone_book[j].find(phone_book[i]) != -1)
 			{
-				return false;
+				if ((phone_book[j].find(phone_book[i]) == 0))
+				{
+					answer = false;
+					break;
+				}
 			}
 		}
 	}
-
-	return true;
+	return answer;
 }
-
 int main()
 {
-	//cout << solution({ "119", "97674223", "1195524421" }) << '\n';
-	//cout << solution({ "123","456","789" })<<'\n';
-	//cout << solution({ "12","123","1235","567","88" })<<'\n';
-	cout << solution({ "123", "456", "4567", "999" }) << '\n';
+	cout << "0: " << solution({ "119", "97674223", "1195524421" }) << '\n';
+	cout << "1: " << solution({ "112","44","4544" }) << '\n';
+	cout << "1: " << solution({ "123","456","789" }) << '\n';
+	cout << "0: " << solution({ "12","123","1235","567","88" }) << '\n';
+	cout << "0: " << solution({ "123", "456", "4567", "999" }) << '\n';
+
 	return 0;
 }
