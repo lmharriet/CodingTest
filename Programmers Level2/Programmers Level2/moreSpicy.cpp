b029f086b333333d3ac076ce48e13c47dcefd711
Harriet Lee <lharriet27@gmail.com>
2021-04-24 15:29:19 +0900

    try to resolve ( still failure)

--- Programmers Level2/Programmers Level2/moreSpicy.cpp
+++ Programmers Level2/Programmers Level2/moreSpicy.cpp
@@ -7,46 +7,38 @@ int solution(vector<int> scoville, int K) {
 	int answer = 0;
 	int count = 0;
 	int minScovile;
-	priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(),scoville.end()); 
+	priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
 
-	while (!pq.empty())
+	if (K == 0) return 0;
+	while (pq.top() < K)
 	{
-		count++;
-		if (pq.top() < K)
+		minScovile = pq.top();
+		pq.pop();
+
+		if (!pq.empty())
 		{
-			minScovile = pq.top();
+			minScovile = minScovile + pq.top() * 2;
 			pq.pop();
 
-			if (!pq.empty())
+			if (minScovile < K)
 			{
-				minScovile = minScovile + pq.top() * 2;
-				pq.pop();
-
-				if (minScovile < K)
-				{
-					pq.push(minScovile);
-				}
-				answer++;
-
+				pq.push(minScovile);
 			}
-			else
-			{
-				return -1;
-			}
+			answer++;
 
 		}
 		else
 		{
-			return answer;
+			return -1;
 		}
 
-		if (count == scoville.size()-1 && answer == 0) return -1;
 	}
-
+	return answer;
 }
 
 int main()
 {
-	cout << solution({ 1,2,3,9,10,12 },1000);
+	cout << solution({ 1,2,3,9,10,12 }, 7);
+	cout << solution({ 2,3,7,10,15 }, 7);
 	return 0;
 }
